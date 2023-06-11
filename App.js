8e67c9ada17705dba00da9ac51d5d6eb3b6235f1
Lourival87 <102527848+Lourival87@users.noreply.github.com>
2023-06-10 22:33:24 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,57 @@
+import React from "react";
+/* import { StatusBar } from 'expo-status-bar';
+import { StyleSheet } from 'react-native'; */
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import UserList from "./src/views/UserList";
+import UserForm from "./src/views/UserForm";
+import { Button, Icon } from "react-native-elements";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="UserList"
+        screenOptions={screenOptions}
+      >
+        <Stack.Screen
+          name="UserList"
+          component={UserList}
+          options={({ navigation }) => {
+            return {
+              title: "Lista de Usuários",
+              headerRight: () => (
+                <Button
+                  onPress={() => navigation.navigate("UserForm")}
+                  type="clear"
+                  icon={<Icon name="add" size={30} color="#fff" />}
+                />
+              ),
+            };
+          }}
+        />
+
+        <Stack.Screen
+          name="UserForm"
+          component={UserForm}
+          options={{
+            title: "Formulário de Usuários",
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const screenOptions = {
+  headerStyle: {
+    backgroundColor: "#faf",
+  },
+  headerTintColor: "#fff",
+  headerTitleStyle: {
+    fontWeight: "bold",
+  },
+};
